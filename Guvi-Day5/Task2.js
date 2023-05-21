3115ee816f924ad54c655249f3fb8ab14407d41c
Ashwin sivasankar v <109450234+Ashwin20BCE1804@users.noreply.github.com>
2023-05-21 10:46:19 +0530

    Add files via upload

---
+++ Guvi-Day5/Task2.js
@@ -0,0 +1,55 @@
+//own resume data using JSON
+let myResume={
+    "basics": {
+      "name": "Ashwin sivasankar v",
+      "email": "ashwin.vitcse@gamil.com",
+      "phone": 9444072249,
+      "degree": "B.Tech CSE",
+      "location": {
+        "address": "o-12,May flower garden,RA puram",
+        "postalCode": 600028,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "www.linkedin.com/in/ashwinsivasankar-venkatesan",
+          "github":"https://github.com/Ashwin20BCE1804/"
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "Vellore institure of technology chennai",
+        "department": "Computer Science",
+        "studyType": "fulltime",
+        "batch start year": 2020,
+        "batch end year": 2024,
+        "gpa": 8.3,
+      }
+    ],
+    "skills": [
+      {
+        "name": "java,c++,html,css,javascript,sql",
+        "level": "average",
+        "project": [
+          "food order website",
+          "travel website"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "web designer,networking,cloud computing",
+      }
+    ]
+  }
+  console.log(myResume);
+  
